4cf4077e8700734770bb883d7a9628416ee270f3
AdeebHaider137 <kaxmiadeeb@gmail.com>
2024-09-10 22:37:33 +0530

    raw test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from the Dockerized Node.js app!');
+    res.send('Helooooooooooooooooooooolo from the Dockerized Node.js app!');
 });
 
 app.listen(PORT, () => {
